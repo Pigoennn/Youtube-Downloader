366e46d563516dee283b75d9b243be060f0c0c0d
Jono-Uni <JSUMANTRI@student.unimelb.edu.au>
2026-07-07 01:49:36 +1000

    add: created main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from downloader import Downloader
+from downloaderUI import YouTubeDownloaderUI
+from PyQt6.QtWidgets import QApplication
+import configparser
+import sys
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    # check if download path is listed
+    outputPath = config["LOCATIONS"]["download_location"]
+    # check if ffmpeg.exe path is listed
+    ffmpegPath = config["LOCATIONS"]["ffmpeg_location"]
+
+    window = YouTubeDownloaderUI(Downloader(outputPath, ffmpegPath))
+    window.show()
+    exit(app.exec())
